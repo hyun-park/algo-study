2ab87c1019e4ba479ca61cc25c15dc06bc1cf8d6
hyun-park <rockethyun@gmail.com>
2019-03-31 16:55:54 +0900

    JUMPGAME solved

--- dynamic_programming/8_JUMPGAME.py
+++ dynamic_programming/8_JUMPGAME.py
@@ -1,28 +1,40 @@
-import numpy as np
+# https://algospot.com/judge/problem/read/JUMPGAME
 
-def jumpgameSolve(board):
-	state_board = np.full((len(board), len(board[0])), -1)
-	np_board = np.array(board)
+cache = dict()
 
-	return recur(state_board, np_board, 0,0)
+def jumpgame(board):
+	if recur(board, 0, 0):
+		return "YES"
+	else:
+	 	return "NO"
 
-def recur(state_board, np_board, y, x):
-	if y >= len(np_board) or x >= len(np_board):
-		return 0
-	if np_board[y,x] == "0":
-		return 1
-	if state_board[y,x] != -1:
-	 	return int(state_board[y,x])
-	jump_size = int(np_board[y,x])
-	state_board[y,x] = recur(state_board, np_board, y + jump_size, x) or recur(state_board, np_board, y, x + jump_size)
-	return int(state_board[y,x])
+def recur(board, y, x):
+	global cache
 
+	if y >= len(board.values()) or x >= len(board.values()):
+		return False
+	if board[y][x] == 0:
+	 	return True
+
+	if y in cache:
+		if x in cache[y]:
+			return cache[y][x]
+		else:
+			cache[y][x] = recur(board, y + board[y][x], x) or recur(board, y, x + board[y][x])
+			return cache[y][x]
+	else:
+		cache[y] = {}
+		cache[y][x] = recur(board, y + board[y][x], x) or recur(board, y, x + board[y][x])
+		return cache[y][x]
 
 if __name__ == '__main__':
-	size = input()
-	board = []
-	for i in range(int(size)):
-		row = input()
-		board.append(row.split(" "))
-	print(jumpgameSolve(board))
+	case = input()
+	for c in range(int(case)):
+		size = input()
+		board = {}
+		for i in range(int(size)):
+			row = input()
+			board[i] = list(map(lambda x: int(x), row.split(" ")))
+		print(jumpgame(board))
+		cache = dict()
 
